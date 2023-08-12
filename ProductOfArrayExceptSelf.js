a02c9f30f5e4a87146f4ddbc4e08c6e6c8e36083
Patrick Callahan <114837562+pcallahandoescs@users.noreply.github.com>
2023-08-11 22:37:48 -0700

    Add files via upload

---
+++ ProductOfArrayExceptSelf.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = (nums) => {
+    let output = new Array(nums.length).fill(nums[0]);
+    let cacheVar = nums[nums.length-1];
+
+    for(let i = 1; i < nums.length; i++){
+        output[i] = output[i-1]*nums[i];
+    }
+
+    output[nums.length-1] = output[output.length-2];
+    for (let j = nums.length-2; j > 0; j--){
+        output[j] = output[j-1]*cacheVar;
+        cacheVar = cacheVar*nums[j]
+    }
+    output[0] = cacheVar;
+    return output;
+};
