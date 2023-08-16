a97f4f1f1c8606f1c123fe325caf9cad0b7f7c53
Patrick Callahan <114837562+pcallahandoescs@users.noreply.github.com>
2023-08-15 21:49:18 -0700

    Add files via upload

---
+++ SearchRotatedSortedArray-1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = (nums, target) => {
+    return nums.indexOf(target);
+};
+
+//this code runs and is by all means technically correct
